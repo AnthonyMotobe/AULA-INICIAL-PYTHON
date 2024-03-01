88007a85b76ead68fc6663b32b6481b6cfb27651
Anthony Motobe <anthonymotobe97@gmail.com>
2024-03-01 20:46:36 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,98 @@
+'''frase = "hello World!"
+primeiro_numero = 1
+segundo_numero = 1.5
+booleana = False
+print(frase)
+print(type(frase))
+print(primeiro_numero)
+print(type(primeiro_numero))
+print(segundo_numero)
+print(type(segundo_numero))
+print(booleana)
+print(type (booleana))
+'''
+'''primeiro_numero = 2
+segundo_numero = 3
+soma = primeiro_numero + segundo_numero
+print(f"A soma entre {primeiro_numero} e {segundo_numero} é {soma}")'''
+'''
+primeiro_numero = 13
+segundo_numero =  76
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+divisao = primeiro_numero / segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+print(f"A soma entre {primeiro_numero} e {segundo_numero} é {soma}")
+print(f"a subtração entre {primeiro_numero} e {segundo_numero} é {subtracao}")
+print(f"a divisão entre {primeiro_numero} e {segundo_numero} é {divisao}")
+print(f"a multiplicação entre {primeiro_numero} e {segundo_numero} é {multiplicacao}")'''
+
+'''primeiro_numero = (input("Diga um numero : ")
+segundo_numero = (input("diga um numero : ")
+soma = primeiro_numero + segundo_numero
+print(f'A soma entre {primeiro_numero} e {segundo_numero} é {soma}')'''
+'''
+primeiro_numero = int(input("Diga um numero : "))
+segundo_numero = int(input("diga um numero : "))
+soma = primeiro_numero + segundo_numero
+print(f'A soma entre {primeiro_numero} e {segundo_numero} é {soma}')'''
+
+'''nome = str(input("Olá digite seu nome:"))
+
+primeiro_numero = int(input(f"{nome} Diga um numero : "))
+segundo_numero = int(input(f"{nome} Diga um numero : "))
+soma = primeiro_numero + segundo_numero
+print(f'A soma entre {primeiro_numero} e {segundo_numero} é {soma}')
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+divisao = primeiro_numero / segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+print(f"A soma entre {primeiro_numero} e {segundo_numero} é {soma}")
+print(f"a subtração entre {primeiro_numero} e {segundo_numero} é {subtracao}")
+print(f"a divisão entre {primeiro_numero} e {segundo_numero} é {divisao}")
+print(f"a multiplicação entre {primeiro_numero} e {segundo_numero} é {multiplicacao}")
+print(f"{nome} esses são os resultados")'''
+
+'''frase = "Hello"
+frase = frase + " World"
+print (frase)'''
+
+'''frase = "Anthony"
+print(frase)
+frase = frase + " Komatsubara"
+print(frase)
+frase = frase + " Motobe"
+print(frase)'''
+
+'''nome = str(input("Olá digite seu nome:"))
+nome ="Olá "+nome+" tudo bem?"
+
+print (nome)
+'''
+
+'''a = 3
+b = 5
+aux = a
+a = b
+b = aux
+print (a)
+print (b)'''
+
+'''a = 2>3
+b = 2==3
+c = 2!=3
+d = not 5==6
+e = 'd' in 'danilo'
+f = 'd' not in 'danilo'
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)'''
+
+
+
+
+
+
